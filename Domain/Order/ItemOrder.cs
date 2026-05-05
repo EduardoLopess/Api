8c7e5e141e1c1ace008be6f670529070b6d5bbfc
Edu <Edu@DESKTOP-7J7UPFP>
2026-05-05 02:56:24 -0300

    product e order#

---
+++ Domain/Order/ItemOrder.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Order
+{
+    public class ItemOrder
+    {
+        public int Id { get; private set; }
+        public int ProductId { get; private set; }
+        public int Quantitiy { get; private set; }
+        public decimal UnitPrice { get; private set; }
+        public decimal? Discount { get; private set;  }
+
+         
+        protected ItemOrder () {}
+
+        public void IncrementQuantitiy()
+        {
+            if (Quantitiy >= 99) throw new InvalidOperationException("Quantidade máxima excedida.");
+
+            Quantitiy += 1;
+        }
+
+        public void DecrementQuantity()
+        {
+            if (Quantitiy <= 1) throw new InvalidOperationException("Quantidade minima permitida é 1. ");
+            Quantitiy -= 1;
+        }
+    }
+}
--- Domain/Product/Additional.cs
+++ Domain/Product/Additional.cs
@@ -12,7 +12,56 @@ namespace Domain.Product
         public string Name { get; private set; }
         public Availability Availability { get; private set; }
         public Price Price { get; private set;  }
+        public QuantityStock Stock { get; private set; }
 
-        
+        public Additional ()
+        {
+
+        }
+
+        public void AddStock(QuantityStock quantityToAdd)
+        {
+            if (quantityToAdd is null)
+                throw new ArgumentNullException(nameof(quantityToAdd), "Valor estoque não informado.");
+
+            Stock = Stock.AddStock(quantityToAdd);
+
+            if (Stock.HasIsStock() && !Availability.IsAvailable())
+            {
+                Available();
+            }
+        }
+
+        public void DebitStock(QuantityStock quantityToDebit)
+        {
+            if (quantityToDebit is null)
+                throw new ArgumentNullException(nameof(quantityToDebit), "Valor a ser decrementado não informado.");
+
+            Stock = Stock.DebitStock(quantityToDebit);
+
+            if (Stock.StockIsEmpty() && Availability.IsAvailable())
+            {
+                Unavailable();
+            }
+
+        }
+
+        private void Unavailable()
+        {
+            if (!Availability.IsAvailable())
+                throw new InvalidOperationException("Produto já indisponível");
+
+            Availability.Unavailable();
+        }
+
+        private void Available()
+        {
+            if (Availability.IsAvailable())
+                throw new InvalidOperationException("Produto já disponível.");
+
+            Availability.Available();
+        }
+
+
     }
 }
